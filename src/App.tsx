f84a1415752bfb7fa64003ff9c9e97138ece0c76
Peter Ye <yaxollum@gmail.com>
2025-05-06 16:58:46 -0400

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 //import { useState } from "react";
-
+import { Button } from "./components/ui/button";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
+      <Button variant="outline">Button</Button>
+
       <p className="text-red-500">Hello world</p>
     </>
   );
